c7b8619dd4e8780c2e6dcb93c8318c2e44357b2b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-12 16:16:33 +0500

    added router and modified file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
-  return <div>Hello CP Website</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
